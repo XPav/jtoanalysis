e46f4f765057b124a53d9f36158ed3a08b907067
Alex Pavloff <alex@augmntr.com>
2020-03-26 11:52:22 -0700

    putting the yasb url in there now also

--- getxws.py
+++ getxws.py
@@ -4,18 +4,28 @@ import os.path
 from os import walk
 import json
 
-def savetoxws( filename, yasburl ):
-    if not os.path.isfile(filename):
-        with open(filename, 'wb') as xws:
+def savetoxws( path, name, number, yasburl ):
+    filename = path + name + ' ' + str(number) + '.json' 
+    if not os.path.isfile( filename ):
+        with open(filename, 'w') as xws:
             print(f'saving {filename} from {yasburl}')
             newurl = yasburl.replace( 'raithos.github.io', 'squad2xws.herokuapp.com/yasb/xws')
+
+            j = {}
+            j['name'] = name
+            j['number'] = number
+            j['yasb'] = yasburl
+
             with urllib.request.urlopen( newurl ) as response:
-                xws.write( response.read() )
+                j['xws'] = json.loads( response.read() )       
+
+            xws.write( json.dumps(j, sort_keys=True, indent=4, separators=(',', ': ')) )
+
 
 
 def saverowtoxws( row ):
-    savetoxws( './xws/' + row[0] + ' 1.json', row[1] )
-    savetoxws( './xws/' + row[0] + ' 2.json', row[2] )
+    savetoxws( './xws/', row[0], 1, row[1] )
+    savetoxws( './xws/', row[0], 2, row[2] )
 
 
 # get all the XWS if we don't have them
@@ -23,7 +33,6 @@ with open('responses.csv', 'r') as csvfile:
     reader = csv.reader(csvfile, delimiter=',' )
     for row in reader:
         saverowtoxws(row)
-
 
 # combine them all now
 xwsfiles = []
@@ -36,15 +45,8 @@ combined = []
 
 for xwsfile in xwsfiles:
     with open(xwsfile,'r') as xws:
-
         name = os.path.splitext( os.path.basename(xwsfile) )[0]
-
-        thislist = {}
-        thislist['name'] = name[0:-2]
-        thislist['number'] = name[-1]
-        thislist['xws'] = json.load(xws)
-
-        combined.append(thislist)
+        combined.append(json.load(xws))
 
 
 with open('combined.json', 'w') as output:
