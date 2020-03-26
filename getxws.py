a40ff94759f675c861f2cb31821f60461214802d
Alex Pavloff <alex@augmntr.com>
2020-03-26 11:23:30 -0700

    made combined

--- getxws.py
+++ getxws.py
@@ -1,6 +1,8 @@
 import csv
 import urllib.request
 import os.path
+from os import walk
+import json
 
 def savetoxws( filename, yasburl ):
     if not os.path.isfile(filename):
@@ -15,7 +17,35 @@ def saverowtoxws( row ):
     savetoxws( './xws/' + row[0] + ' 1.json', row[1] )
     savetoxws( './xws/' + row[0] + ' 2.json', row[2] )
 
+
+# get all the XWS if we don't have them
 with open('responses.csv', 'r') as csvfile:
     reader = csv.reader(csvfile, delimiter=',' )
     for row in reader:
         saverowtoxws(row)
+
+
+# combine them all now
+xwsfiles = []
+for (dirpath, dirnames, filenames) in walk('./xws/'):
+    for filename in filenames:
+        xwsfiles.append ( os.path.join( dirpath, filename ) )
+
+
+combined = []
+
+for xwsfile in xwsfiles:
+    with open(xwsfile,'r') as xws:
+
+        name = os.path.splitext( os.path.basename(xwsfile) )[0]
+
+        thislist = {}
+        thislist['name'] = name[0:-2]
+        thislist['number'] = name[-1]
+        thislist['xws'] = json.load(xws)
+
+        combined.append(thislist)
+
+
+with open('combined.json', 'w') as output:
+    output.write( json.dumps(combined, sort_keys=True, indent=4, separators=(',', ': '))) 
